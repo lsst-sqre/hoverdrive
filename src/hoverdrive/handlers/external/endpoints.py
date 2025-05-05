62bd04ff34e5e7413f98dba0d784f0e4edf0937f
Jonathan Sick <jsick@lsst.org>
2025-05-05 16:45:25 -0400

    Create endpoints dedicated to redirects
    We found that we couldn't describe an optional ?redirect=true flag in
    the VO service descriptors (as well as the subtlety of multiple
    columns/tables not being allowed in that context). Therefore I've pulled
    out the redirect functionality into new `GET
    /hoverdrive/column-docs-redirect` and `GET
    /hoverdrive/table-docs-redirect` endpoints
    
    Since VO table results aren't implemented yet, I've deleted the
    original endpoints. They will be re-added though once we know the schema
    for VO tables of documentation links.

--- src/hoverdrive/handlers/external/endpoints.py
+++ src/hoverdrive/handlers/external/endpoints.py
@@ -4,15 +4,10 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Query, Request
 from fastapi.responses import RedirectResponse
-from safir.models import ErrorLocation
 from safir.slack.webhook import SlackRouteErrorHandler
 
 from hoverdrive.dependencies.context import RequestContext, context_dependency
-from hoverdrive.exceptions import (
-    EndpointNotImplementedError,
-    LinkRedirectRequestError,
-    NotFoundError,
-)
+from hoverdrive.exceptions import NotFoundError
 
 from .models import Index
 
@@ -35,10 +30,10 @@ async def get_index(request: Request) -> Index:
 
 
 @router.get(
-    "/column-docs-links",
-    summary="Get documention links for a column",
+    "/column-docs-redirect",
+    summary="Redirect to the most relevant documentation link for a column",
 )
-async def get_column_docs_links(
+async def get_column_docs_redirect(
     *,
     table_name: Annotated[
         str,
@@ -49,82 +44,46 @@ async def get_column_docs_links(
             examples=["dp02_dc2_catalogs.Object"],
         ),
     ],
-    column_names: Annotated[
-        list[str],
+    column_name: Annotated[
+        str,
         Query(
             ...,
             alias="column",
             title="Column name",
-            description=(
-                "The name of the column to get links for. Provide multiple "
-                "column parameters to get links for multiple columns."
-            ),
             examples=["detect_isPrimary"],
         ),
     ],
-    redirect: Annotated[
-        bool,
-        Query(
-            title="Redirect",
-            description="Whether to redirect to the documentation link.",
-        ),
-    ] = False,
     context: Annotated[RequestContext, Depends(context_dependency)],
 ) -> RedirectResponse:
-    """Use a `redirect=true` query parameter to get a redirect to the most
-    documentation link for this column.
-    """
     factory = context.factory
     links_service = factory.get_links_service()
-    if redirect:
-        if len(column_names) != 1:
-            raise LinkRedirectRequestError(
-                "Only one column name is supported for redirection.",
-                ErrorLocation.query,
-                ["column"],
-            )
-        column_name = column_names[0]
-        link = await links_service.get_redirect_link_for_column(
-            table_name, column_name
-        )
-        if link is None:
-            raise NotFoundError(
-                "No documentation link found for column "
-                f"{table_name}.{column_name}"
-            )
-        return RedirectResponse(url=link, status_code=307)
 
-    raise EndpointNotImplementedError(
-        "Use of `redirect=true` is required for this endpoint."
+    link = await links_service.get_redirect_link_for_column(
+        table_name, column_name
     )
+    if link is None:
+        raise NotFoundError(
+            "No documentation link found for column "
+            f"{table_name}.{column_name}"
+        )
+    return RedirectResponse(url=link, status_code=307)
 
 
 @router.get(
-    "/table-docs-links",
-    summary="Get documention links for a table",
+    "/table-docs-redirect",
+    summary="Redirect to the most relevant documentation link for a table",
 )
-async def get_table_docs_links(
+async def get_table_docs_redirect(
     *,
-    table_names: Annotated[
-        list[str],
+    table_name: Annotated[
+        str,
         Query(
             ...,
             alias="table",
             title="Table name",
-            description=(
-                "The name of the table to get links for. Provide multiple "
-                "table parameters to get links for multiple tables."
-            ),
             examples=["dp02_dc2_catalogs.Object"],
         ),
     ],
-    redirect: Annotated[
-        bool,
-        Query(
-            title="Redirect",
-            description="Whether to redirect to the documentation link.",
-        ),
-    ] = False,
     context: Annotated[RequestContext, Depends(context_dependency)],
 ) -> RedirectResponse:
     """Use a `redirect=true` query parameter to get a redirect to the most
@@ -132,21 +91,9 @@ async def get_table_docs_links(
     """
     factory = context.factory
     links_service = factory.get_links_service()
-    if redirect:
-        if len(table_names) != 1:
-            raise LinkRedirectRequestError(
-                "Only one table name is supported for redirection.",
-                ErrorLocation.query,
-                ["table"],
-            )
-        table_name = table_names[0]
-        link = await links_service.get_redirect_link_for_table(table_name)
-        if link is None:
-            raise NotFoundError(
-                f"No documentation link found for table {table_name}"
-            )
-        return RedirectResponse(url=link, status_code=307)
-
-    raise EndpointNotImplementedError(
-        "Use of `redirect=true` is required for this endpoint."
-    )
+    link = await links_service.get_redirect_link_for_table(table_name)
+    if link is None:
+        raise NotFoundError(
+            f"No documentation link found for table {table_name}"
+        )
+    return RedirectResponse(url=link, status_code=307)
